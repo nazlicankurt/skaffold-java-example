d9aca88d9f8b426dbb929badf37d101a18211e92
Nazlican Kurt <kurtnazlican96@gmail.com>
2021-10-27 23:24:46 +0300

    skaffold-mini-kubernetes

---
+++ skaffold-example/src/main/java/com/example/skaffoldexample/SkaffoldExampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.skaffoldexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkaffoldExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkaffoldExampleApplication.class, args);
+	}
+
+}
---
+++ skaffold-example/src/test/java/com/example/skaffoldexample/SkaffoldExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.skaffoldexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkaffoldExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
